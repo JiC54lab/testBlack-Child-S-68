168d50cc43fd97365c1bb7e730ea04a68b5912d7
JiC54lab <105932652+JiC54lab@users.noreply.github.com>
2022-06-21 00:05:27 +0200

    Update url_shortner.py

--- plugins/url_shortner.py
+++ plugins/url_shortner.py
@@ -14,6 +14,7 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import os
+import pySmartDL
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InlineQueryResultArticle, InputTextMessageContent
 from pyrogram.handlers import MessageHandler
